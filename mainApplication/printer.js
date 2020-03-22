6bb7ff4c7f2bc431db830db92f131bf673cc2102
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-03-22 00:03:18 -0600

    Refactoring the Amount calculation in the switch.

--- mainApplication/printer.js
+++ mainApplication/printer.js
@@ -10,28 +10,9 @@ module.exports = function statement(invoice, plays) {
                             minimumFractionDigits: 2}).format;
 
      for (let perf of invoice.performances) {
-        const play = plays[perf.playID];;
-        let thisAmount = 0;
-        
-        switch (play.type) {
-            case "tragedy":
-                thisAmount = 40000;
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-                
-            case "comedy":
-                thisAmount = 30000;
-                if(perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-        
-            default:
-                throw new Error(`unkown type: ${play.type}`);
-        }
+        const play = plays[perf.playID];
+
+        let thisAmount = amountFor(perf, play);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -46,6 +27,32 @@ module.exports = function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount/100)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
+}
+
+function amountFor(perf, play){    
+    let thisAmount = 0;
+    
+    switch (play.type) {
+        case "tragedy":
+            thisAmount = 40000;
+            if(perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+            
+        case "comedy":
+            thisAmount = 30000;
+            if(perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+    
+        default:
+            throw new Error(`unkown type: ${play.type}`);
+    }
+    
+    return thisAmount;
 }
 
 //console.log(statement(invoices[0], plays));
