96ec4abd4359987a20f4153708f7dfd55edb8415
Marco De Jesus <Marco_De_Jesus@epam.com>
2020-03-22 00:35:29 -0600

    Removing the variable

--- mainApplication/printer.js
+++ mainApplication/printer.js
@@ -10,7 +10,7 @@ module.exports = function statement(invoice, plays) {
                             minimumFractionDigits: 2}).format;
 
      for (let perf of invoice.performances) {
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -30,7 +30,7 @@ module.exports = function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }
 
-    function amountFor(aPerformance, play){    
+    function amountFor(aPerformance){    
         let result = 0;
         
         switch (playFor(aPerformance).type) {
